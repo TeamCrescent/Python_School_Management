a820dbe43871f01e4a9f72b3c359adaaf8d08d6a
MrChike <chikeegonu@gmail.com>
2019-10-30 19:08:48 +0100

    update

---
+++ notes.py
@@ -0,0 +1,1 @@
+# Def 1 - A value holder for strings variables - method for tkinter
--- program.py
+++ program.py
@@ -11,3 +11,31 @@ class Student:
     def __init__(self, root):
         self.root = root
         self.root.title("Student Database Management System")
+        self.root.geometry("1350x750+0+0")
+        self.root.config(bg="cadet blue")
+
+        StdID = StringVar()  # Def 1
+        Firstname = StringVar()
+        Surname = StringVar()
+        DoB = StringVar()
+        Age = StringVar()
+        Gender = StringVar()
+        Address = StringVar()
+        Mobile = StringVar()
+
+        # -------------------------------Frames---------------------------------
+        # MainFrame = Frame(self, root, bg="cadet blue")
+        # MainFrame.grid()
+        #
+        # TitFrame = Frame(MainFrame, bd=2, padx=54, pady=8, bg="Ghost White", relief=RIDGE)
+        # TitFrame.pack(side=TOP)
+        #
+        # self.iblTit = Label(TitFrame, font=('arial', 47, 'bold'),
+        #                     text="Student Database Management Systems", bg="Ghost White")
+        # self.iblTit.grid()
+
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    application = Student(root)
+    root.mainloop()
