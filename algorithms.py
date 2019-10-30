decf71a550b7fea3d816a5133b2bbb74ccfe0921
MrChike <chikeegonu@gmail.com>
2019-10-30 16:30:36 +0100

    tkinter testing

---
+++ algorithms.py
@@ -0,0 +1,3 @@
+# confirm if tkinter is installed on system
+#   python -m tikinter
+#   https://docs.python.org/3/library/tkinter.html
---
+++ program.py
@@ -0,0 +1,13 @@
+# Frontend
+
+from tkinter import *
+import tkinter.messagebox
+
+# import Student's Database
+
+
+class Student:
+
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Student Database Management System")
